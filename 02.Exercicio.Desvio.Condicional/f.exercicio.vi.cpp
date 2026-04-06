24da991f7983e71d3554f38eaf24c3329d9a3a92
Gabriel Alves Jacques <ga289el.ti@gmail.com>
2026-04-06 20:28:01 -0300

    Update comments for clarity in f.exercicio.vi.cpp

--- 02.Exercicio.Desvio.Condicional/f.exercicio.vi.cpp
+++ 02.Exercicio.Desvio.Condicional/f.exercicio.vi.cpp
@@ -1,5 +1,6 @@
-//Faça um programa que exiba o maior dentre dois números reais digitados pelo usuário. Caso
-//eles sejam iguais exiba uma mensagem correspondente. Saídas:
-//• Pedido ao usuário = "Digite dois números reais:";
-//• Caso eles sejam iguais = “Eles são iguais”.
-//• Caso sejam diferentes exiba somente o numero desejado.
+//Faça um programa que exiba o maior dentre dois números reais digitados pelo usuário.
+//Caso eles sejam iguais exiba uma mensagem correspondente. Saídas:
+//- Pedido ao usuário = "Digite dois números reais:";
+//- Caso eles sejam iguais = “Eles são iguais”.
+//- Caso sejam diferentes exiba somente o numero desejado.
+
