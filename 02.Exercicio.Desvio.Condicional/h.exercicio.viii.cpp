37e8d286363019e0b54d391536f3fda841dc241f
Gabriel Alves Jacques <ga289el.ti@gmail.com>
2026-04-20 11:02:24 -0300

    Correct logic for finding the smallest of three numbers
    Fix the logic to correctly identify the smallest number.

--- 02.Exercicio.Desvio.Condicional/h.exercicio.viii.cpp
+++ 02.Exercicio.Desvio.Condicional/h.exercicio.viii.cpp
@@ -1,1 +1,23 @@
+//Faça um programa que solicita ao usuário três números reais e exibe na tela apenas o menor deles.
 
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  float n1, n2, n3;
+
+  cout<<"Digite três valores: 0";
+  cin>>n1>>n2>>n3;
+
+  if(n1<n2 && n1<n3){
+    cout<<"O menor numero é: "<<n1;
+  } else {
+    if(n2<n1 && n2<<n3){
+    cout<<"O menor numero é: "n2;
+    } else{
+      cout<<"O menor numero é: "n3;
+    }
+    
+    return 0;
+}
