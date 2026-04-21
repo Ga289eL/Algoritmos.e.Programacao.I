b991b175b80abc45136994ad8f5f8d59c367689f
Gabriel Alves Jacques <ga289el.ti@gmail.com>
2026-04-21 19:22:09 -0300

    Create e.exercicio.v.cpp

---
+++ 03.Exercicios.Lacos.de.Repeticao/e.exercicio.v.cpp
@@ -0,0 +1,18 @@
+//Faça um programa que exiba na tela a soma dos números inteiros do intervalo [100, 200].
+//Exemplo: soma = 100 + 101 +102 + ... + 200.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int numero=100, soma=0;
+
+  while(numero<=200){
+    soma+=numero;
+    cout<<"soma = "<<soma<<endl;
+    numero+=1;
+  }
+
+  return 0;
+}
