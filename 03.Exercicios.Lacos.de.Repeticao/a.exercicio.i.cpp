2cf2556f14b28786f857861eec980b105dcb356e
Gabriel Alves Jacques <ga289el.ti@gmail.com>
2026-04-21 16:46:24 -0300

    Display message 30 times using a while loop
    Fix the loop to correctly increment the counter.

--- 03.Exercicios.Lacos.de.Repeticao/a.exercicio.i.cpp
+++ 03.Exercicios.Lacos.de.Repeticao/a.exercicio.i.cpp
@@ -1,1 +1,16 @@
 // Faça um programa que exiba 30 vezes na tela a mensagem “Não vou colar na prova”.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int contador=1;
+
+  while(contador<=30){
+    cout<<"Não vou colar na prova"<<endl;
+    contador = contador+1;
+  }
+
+  return 0;
+}
