e99c718831668dd333b26695276e6232409840c2
Gabriel Alves Jacques <ga289el.ti@gmail.com>
2026-04-27 19:46:13 -0300

    Update m.exercicio.cpp

--- 03.Exercicios.Lacos.de.Repeticao/m.exercicio.cpp
+++ 03.Exercicios.Lacos.de.Repeticao/m.exercicio.cpp
@@ -1,1 +1,14 @@
+//Faça um algoritmo que leia um conjunto de 20 números inteiros e indique, ao final, qual foi o menor valor digitado.
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int cont=1, numeros;
+
+    while(cont=<20){
+        cout<<cont;
+    }
+  
+}
