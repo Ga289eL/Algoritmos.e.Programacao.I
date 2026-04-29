1271d25578d4506524272b7ec4d5517dae67a400
Gabriel Alves Jacques <ga289el.ti@gmail.com>
2026-04-29 14:18:42 -0300

    Generate first 20 terms of Fibonacci series
    Fix Fibonacci series calculation and output format.

--- 03.Exercicios.Lacos.de.Repeticao/j.exercicio.x.cpp
+++ 03.Exercicios.Lacos.de.Repeticao/j.exercicio.x.cpp
@@ -1,1 +1,21 @@
+//Faça um programa que gere e exiba os 20 primeiros termos da série de Fibonacci
+//(Os dois primeiros termos da série são 1 e 1, os termos subsequentes são a soma dos dois últimos.
+//Observe o exemplo: 1,1,2,3,5,8,13,21,..)
 
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+    int var1=1, var2=1;
+
+    int i;
+    for(i=1; i<=10; i++){
+        cout<< var1 <<" - "<< var2 <<" - ";
+        var1 += var2;
+        var2 += var1;
+    }
+
+    return 0;
+  
+}
