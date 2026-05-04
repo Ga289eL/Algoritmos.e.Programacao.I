4848389025e4a2733c503d37441735803589ae84
Gabriel Alves Jacques <ga289el.ti@gmail.com>
2026-05-04 00:56:38 -0300

    Update l.exercicio.xii.cpp

--- 03.Exercicios.Lacos.de.Repeticao/l.exercicio.xii.cpp
+++ 03.Exercicios.Lacos.de.Repeticao/l.exercicio.xii.cpp
@@ -1,1 +1,25 @@
+//Faça um programa que solicita a idade de 10 pessoas e exiba a quantidade de pessoas que possui idade maior ou igual a 18 anos.
 
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int i, idade, maior=0;
+
+    for(i=1;i<=10;i++){
+
+        cout<<"Digite a idade da "<<i<<"ª pessoa: ";
+        cin>>idade;
+
+        if(idade>17){
+            maior+=1;
+        }
+      
+    }
+
+    cout<<"A quantidade de pessoas com idade maior ou igual a 18 é "<<maior;
+
+    return 0;
+
+}
