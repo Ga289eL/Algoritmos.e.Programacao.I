7305307373d9d698ca0d8b1e6b163082d2e00b4b
Gabriel Alves Jacques <ga289el.ti@gmail.com>
2026-05-04 19:44:25 -0300

    Create p.exercicio.xvi.cpp

---
+++ 03.Exercicios.Lacos.de.Repeticao/p.exercicio.xvi.cpp
@@ -0,0 +1,21 @@
+//Solicite ao usuário a digitação de um número inteiro, calcule e exiba o fatorial deste número
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int fatorial, numero;
+
+    cout<<"Digite o numero a ser fatordo: ";
+    cin>>numero;
+
+    for(fatorial=1;numero>1;numero--){
+      fatorial*=numero;
+    }
+
+    cout<<"o fatorial do numero digitado é "<<fatorial;
+    
+    return 0;
+  
+}
