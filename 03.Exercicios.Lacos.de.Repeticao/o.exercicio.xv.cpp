dd24a82f35413ce73a204872063cbe2566fc75cf
Gabriel Alves Jacques <ga289el.ti@gmail.com>
2026-05-06 10:18:43 -0300

    Update o.exercicio.xv.cpp

--- 03.Exercicios.Lacos.de.Repeticao/o.exercicio.xv.cpp
+++ 03.Exercicios.Lacos.de.Repeticao/o.exercicio.xv.cpp
@@ -1,1 +1,24 @@
+//Faça um programa que solicita 20 valores inteiros e exiba quantos são pares e quantos são ímpares.
 
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int numero, par=0, impar=0;
+
+    int i;
+    for(i=1; i <= 20; i++){ //n�mero de valores
+        cout << "Digite o " << i << "º numero: ";
+        cin >> numero;
+        if (numero % 2 == 0){
+            par++;
+        }else{
+            impar++;
+        }
+    }
+    cout << "\nO total de numeros pares foi de: " << par;
+    cout << "\nO total de numeros impares foi de: " << impar;
+
+    return 0;
+}
