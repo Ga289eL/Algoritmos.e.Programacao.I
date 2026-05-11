c62d00b09fb6015a3789e626e6a2fa6813ee7648
Gabriel Alves Jacques <ga289el.ti@gmail.com>
2026-05-11 14:19:56 -0300

    Update i.exercicio.ix.cpp

--- 03.Exercicios.Lacos.de.Repeticao/i.exercicio.ix.cpp
+++ 03.Exercicios.Lacos.de.Repeticao/i.exercicio.ix.cpp
@@ -12,39 +12,45 @@ int main() {
     float operando1, operando2, resultado;
 
     do{
-        cout << "Digite um valor para o operando 1: ";
-        cin >> operando1;
+        cout<<"Digite um valor para o operando 1: ";
+        cin>>operando1;
 
-        cout << "Digite um valor para o operando 2: ";
-        cin >> operando2;
+        cout<<"Digite um valor para o operando 2: ";
+        cin>>operando2;
 
-        if (operando1 != 0 || operando2 != 0){
-            cout << "Escolha a operação:\n1- soma\n2- produto\n3- divis�o\n4- pot�ncia\n-> "; //exibe menu de op��es
-            cin >> operador;
+        if (operando1!=0 || operando2!=0){
+            cout<<"Escolha a operação:\n1- soma\n2- produto\n3- divisão\n4- potência\n-> ";
+            cin>>operador;
 
             switch(operador){
                 case 1:
                     resultado = operando1 + operando2;
                     break;
+                
                 case 2:
                     resultado = operando1 * operando2;
                     break;
+                
                 case 3:
                     resultado = operando1 / operando2;
                     break;
+                
                 case 4:
-                    resultado = operando1;  //atribui base para primeiro c�lculo
-                    if (operando2 == 0)
-                        resultado =1;
+                    resultado = operando1;
+                    if (operando2==0)
+                        resultado=1;
+                    
                     int i;
-                    for (i=1; i<operando2; i++){ //utiliza segundo operando para controlar repeti��es (expoente)
-                        resultado = operando1 * resultado;
+                    
+                    for(i=1; i<operando2; i++){
+                        resultado=operando1*resultado;
                     }
                     break;
             }
-            cout << endl << resultado << endl;
+            
+            cout<<endl<<resultado<<endl;
         }
-    }while (operando1 != 0 || operando2 != 0);
+    }while(operando1 != 0 || operando2 != 0);
 
     return 0;
 }
