d7257f22e025fb1c8584322041a2463eb6c5c14c
Gabriel Alves Jacques <ga289el.ti@gmail.com>
2026-05-18 19:43:10 -0300

    Update a.exercicio.i.cpp

--- 03.Exercicios.Lacos.de.Repeticao/a.exercicio.i.cpp
+++ 03.Exercicios.Lacos.de.Repeticao/a.exercicio.i.cpp
@@ -1,4 +1,4 @@
-// Faça um programa que exiba 30 vezes na tela a mensagem “Não vou colar na prova”.
+//Faça um programa que exiba 30 vezes na tela a mensagem “Não vou colar na prova”.
 
 #include <iostream>
 using namespace std;
