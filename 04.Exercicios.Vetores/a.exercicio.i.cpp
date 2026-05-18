29ef2ad193a27ebfe2450b8104041742d6910026
Gabriel Alves Jacques <ga289el.ti@gmail.com>
2026-05-18 20:42:03 -0300

    Create a.exercicio.i.cpp

---
+++ 04.Exercicios.Vetores/a.exercicio.i.cpp
@@ -0,0 +1,31 @@
+//Declare um vetor de 10 inteiros, leia um valor para cada posição e no final mostre a média, o menor e o maior valor contidos no vetor.
+
+#include <iostream>
+using namespace std;
+
+#define T 10
+
+int main(){
+    int vet[T], max=0, min=0, soma=0;
+    float med=0;
+    
+    for(int i=0; i<T; i++){
+        cout<<"\nDigite o valor: ";
+        cin>>vet[i];
+    }
+    
+    for(int i=0; i<T; i++){
+        soma+=vet[i];
+        
+        if(vet[i]>max)
+            max=vet[i];
+        if(vet[i]<min)
+            min=vet[i];
+    }
+    
+    med= (float) soma/T;
+    
+    cout<<"\nMedia do valor: "<<med<<"\nMenor valor: "<<min<<"\nMaior valor: "<<max;
+    
+    return 0;
+}
