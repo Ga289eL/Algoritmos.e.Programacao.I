1de6219dbad0af2877c4c2996b0b91a9d6ddc8ac
Gabriel Alves Jacques <ga289el.ti@gmail.com>
2026-06-08 16:22:07 -0300

    Create b.exercicio.ii.cpp

---
+++ 05.Exercicios.Matrizes/b.exercicio.ii.cpp
@@ -0,0 +1,24 @@
+//Faça um programa que preencha uma matriz de 5X5 com o elemento um em todas as posições em que o índice de linha tem valor igual ao da coluna.
+
+#include <iostream>
+using namespace std;
+
+#define C 5
+#define L 5
+
+int main(){
+    int mat[L][C]={};
+    
+    for(int i=0;i<L;i++){
+            mat[i][i]=1;
+    }
+    
+    for(int i=0;i<L;i++){
+        for(int j=0;j<C;j++)
+            cout<<"\t"<<mat[i][j]<<"\t";
+            
+        cout<<"\t\n";
+    }
+    
+    return 0;
+}
